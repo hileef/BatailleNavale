76117242e020881b83e7a5158257b63ca29983de
prieliot <eliothautefeuille@wifi-172-30-20-120.wifi.univ-paris5.fr>
2014-10-31 11:51:58 +0100

    Premiers pas

--- cli.c
+++ cli.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void afficherTableau(int** tableau, int taille);
+#define TAILLE 5
+
+void afficherTableau(int tableau[TAILLE][TAILLE], int taille);
 void demander(char* s, char* t, int n);
 void nettoyerAffichage();
 char indiceLettre(int x);
@@ -10,11 +12,21 @@ char indiceLettre(int x);
 static void glup(char* t, int n);
 static void vidange();
 
+static char** EUStringToStringTable(char* s, char sep, int* length);
+static int EUcountSeparators(char* s, int size, char separator);
+
 // LES FONCTIONS CI-DESSOUS SONT DISPONIBLES POUR L'UTILISATEUR
 
 void demander(char* s, char* t, int n) {
 	printf("%s", s);
 	glup(t, n);
+}
+
+char** demanderTableau(char* s, int* n) {
+	printf("%s", s);
+	char target[100];
+	glup(target, 100);
+	return EUStringToStringTable(target ,' ', n);
 }
 
 void nettoyerAffichage() {
@@ -25,19 +37,24 @@ void nettoyerAffichage() {
 	#endif
 }
 
-void afficherTableau(int** tableau, int taille) {
+void afficherTableau(int tableau[TAILLE][TAILLE], int taille) {
 	int i, j;
 
 	printf("    ");
 	for(i = 0; i < taille; i++)
 		printf(" %d ", i);
-	
+	printf("\n    ");
+	for(i = 0; i < taille; i++)
+		printf(" - ");
+	printf("\n");
 	for(i = 0; i < taille; i++) {
-		printf(" %c |", indiceLettre(i));
+		printf(" %c -", indiceLettre(i));
 		for(j = 0; j < taille; j++)
 			printf(" %d ", tableau[i][j]);
-		printf("| %c \n", indiceLettre(i));
+		printf("\n");
 	}
+
+	printf("\n");
 }
 
 char indiceLettre(int x) {
@@ -54,4 +71,44 @@ static void glup(char* t, int n) {
 static void vidange() {
 	char c;
 	while((c = getchar()) != '\0' && c != EOF && c != '\n');
+}
+
+// Parses String to String table using separator sep
+static char** EUStringToStringTable(char* s, char sep, int* length) {
+	int i, j, k, l, m, n, N;
+	char** table;
+	char* word;
+	
+	i = j = k = l = m = n = 0;
+
+	N = strlen(s);
+	n = EUcountSeparators(s, N, sep) + 1;
+	table = (char**) malloc(sizeof(char*) * n);
+	word = (char*) malloc(sizeof(char) * (k+1));
+
+	for(i = 0; i <= N; i++) {
+		j++;
+		if((s[i] == sep || s[i] == '\0') && i != 0) {
+			k = j - 1;
+			word = (char*) malloc(sizeof(char) * (k));
+			for(l = 0; l < k; l++)
+				word[l] =  s[(i - k) + l];
+			word[k] = '\0';
+			table[m] = word;
+			j = 0;
+			m++;
+		}
+	}
+
+	*length = n;
+	return table;
+
+}
+
+// Counts occurence of character separator in string
+static int EUcountSeparators(char* s, int size, char separator) {
+	int i, n;
+	for(i = n = 0; i < size; i++)
+		if(s[i] == separator) n++;
+	return n;
 }
--- jeu.c
+++ jeu.c
@@ -1,14 +1,106 @@
 #include <stdio.h>
 #include "cli.c"
 
+#define TAILLE 5
+#define TAILLE_BATEAU 3
+
+void initialiserTableau(int t[TAILLE][TAILLE]);
+void demanderPlacerBateaux(int bateauxJoueur1[TAILLE][TAILLE]);
+int validerCoordonees(char** coord, int nombre);
+
+
 // Fonction de démarrage.
 int main() {
 
-	char entree[100];
+	int bateauxJoueur1[TAILLE][TAILLE];
+	int tirsJoueur2[TAILLE][TAILLE];
 
-	do {
-		nettoyerAffichage();
-		printf("Bonjour, bienvenue sur notre sublissime jeu.\n");
-		demander("Voulez-vous quitter? Si oui, tapez '0' : ", entree, sizeof entree);
-	} while(entree[0] != '0');
+	initialiserTableau(bateauxJoueur1);
+	initialiserTableau(tirsJoueur2);
+
+	demanderPlacerBateaux(bateauxJoueur1);
+}
+
+void demanderPlacerBateaux(int bateauxJoueur1[TAILLE][TAILLE]){
+	int i;
+	int nombre;
+	char entree[100];
+	char** coordonees;
+
+	afficherTableau(bateauxJoueur1, TAILLE);
+	coordonees = demanderTableau("Veuillez entrer les coordonees de votre bateau en majuscules, séparé par des espaces. (Toutes les cases qu'il prend)(exemple: 'A1 A2 A3') : ", &nombre);
+	
+	if(validerCoordonees(coordonees, nombre))
+		printf("Valide.\n");
+	else
+		printf("PAS Valide.\n");
+
+/*
+	for(i = 0; i < nombre; i++)
+		printf("%s\n", coordonees[i]);
+*/
+
+}
+
+int validerCoordonees(char** coord, int nombre) {
+	// Verfier 1 letter 1 chifffre
+	// Verifier alignés && à la suite
+	// A l'interieur du tableau
+
+	char t;
+	int i;
+	int orientationHorizontale, orientationVerticale, croissant, decroissant;
+	orientationHorizontale = orientationVerticale = croissant = decroissant = 1;
+
+	printf("Verification: Lettre et Chiffre dans tableau\n");
+	for(i = 0; i < nombre; i++){
+		char c1 = coord[i][0];
+		char c2 = coord[i][1];
+		if(c1 < 'A' && c1 >= 'A' + TAILLE) return 0;
+		if(c2 < '0' && c2 > '9') return 0;
+	}
+
+	t = coord[0][0];
+	for(i = 1; i < nombre; i++){
+		if(t != coord[i][0]) orientationHorizontale = 0; 
+	}
+
+	t = coord[0][1];
+	for(i = 1; i < nombre; i++){
+		if(t != coord[i][1]) orientationVerticale = 0;
+	}
+
+	printf("Verification: Orientation stable\n");
+	if(!orientationVerticale && !orientationHorizontale)
+		return 0;
+
+	if(orientationHorizontale) {
+		char u = coord[0][1];
+		for(t = 1; i < nombre; i++) {
+			if(coord[i][1] != u + i) croissant = 0;
+			if(coord[i][1] != u - i) decroissant = 0;
+		}
+	}
+	printf("Verification: Croissant:%d, Decroissant:%d \n", croissant, decroissant);
+
+	if(orientationVerticale) {
+		char u = coord[0][0];
+		for(t = 1; i < nombre; i++) {
+			if(coord[i][0] != u + i) croissant = 0;
+			if(coord[i][0] != u - i) decroissant = 0;
+		}
+	}
+
+	printf("Verification: Coordonees a la suite\n");
+	if(!croissant && !decroissant) return 0;
+
+	return 1;
+
+}
+
+void initialiserTableau(int t[TAILLE][TAILLE]){
+	int i, j;
+	for(i = 0; i < TAILLE; i++)
+		for(j = 0; j < TAILLE; j++)
+			t[i][j] = 0;
 }
