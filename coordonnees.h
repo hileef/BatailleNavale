f4434d467ee53cc8f246428f53c680ab18bad465
killwithabass <acolonna@etu.utc.fr>
2014-11-28 16:46:59 +0100

    Style, nom

--- coordonnees.h
+++ coordonnees.h
@@ -7,6 +7,9 @@ struct Coordonnee {
 };
 typedef struct Coordonnee Coordonnee;
 
+enum Orientation {VERTICAL = 1, HORIZONTAL = 2};
+enum Variation {CROISSANT = 1, DECROISSANT = 2};
+
 void	setY(Coordonnee* c, int y);
 void	setX(Coordonnee* c, int x);
 int 	getY(Coordonnee* c);
@@ -18,20 +21,20 @@ void 	initCoordonnee(Coordonnee* c);
 void 	initCoordonnee(Coordonnee* c, int i, int j);
 bool 	initCoordonnee(Coordonnee* c, const char* texte);
 
-int 	initSuiteCoordonnees(Coordonnee cible[], int n);
-int 	initSuiteCoordonnees(Coordonnee cible[], int n, int i, int j);
-int 	initSuiteCoordonnees(Coordonnee cible[], int n, char** textes);
-int 	initSuiteCoordonnees(Coordonnee cible[], int n, const char* texte);
+int 	initSuiteCoordonnees(Coordonnee tableau_coordonnees[], int n);
+int 	initSuiteCoordonnees(Coordonnee tableau_coordonnees[], int n, int i, int j);
+int 	initSuiteCoordonnees(Coordonnee tableau_coordonnees[], int n, char** textes);
+int 	initSuiteCoordonnees(Coordonnee tableau_coordonnees[], int n, const char* texte);
 
 // Les prototypes privés
 static bool validerCoordonnee(char y, char x, Coordonnee *c);
-static int  sontAlignees(Coordonnee cible[], int nombre);
-static int  sontALaSuite(Coordonnee cible[], int nombre, int orientation);
-static int  validerSuiteCoordonnees(Coordonnee cible[], int nombre);
+static int  orientationAlignement(Coordonnee tableau_coordonnees[], int nombre_coordonnees);
+static int  variationEtOrdre(Coordonnee tableau_coordonnees[], int nombre_coordonnees, int orientation);
+static int  validerSuiteCoordonnees(Coordonnee tableau_coordonnees[], int nombre_coordonnees);
 
-static int  etendueSuiteCoordonnees(char** entree, int nombre);
+static int  etendueSuiteCoordonnees(char** entree, int nombre_coordonnees);
 static int  etendueSuiteCoordonnees(Coordonnee* a, Coordonnee* b);
-static void etendreSuiteCoordonnees(Coordonnee cible[], int etendue);
+static void etendreSuiteCoordonnees(Coordonnee tableau_coordonnees[], int etendue);
 
 // Les tests
 void 	testsCoordonnees();
